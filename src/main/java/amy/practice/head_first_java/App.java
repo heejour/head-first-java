da2913b3ea847288e556672b71c868d37c316444
heejour <cuteprobe@gmail.com>
2018-06-18 19:01:23 +0900

    init

---
+++ src/main/java/amy/practice/head_first_java/App.java
@@ -0,0 +1,13 @@
+package amy.practice.head_first_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
